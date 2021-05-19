fb74614b7f275cf9b3215a5c44afeb4199fe2fdb
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2021-05-20 02:21:11 +0900

    v0.1.0

---
+++ AutoDetectFall.py
@@ -0,0 +1,59 @@
+import cryptofunc
+import pyupbit
+import telegram
+import time
+
+"""
+AutoDetectFall.py
+* Date: 2021.05.20.
+* Author: Jeon Won
+* Func: 가상화폐 급락했을 때(현재가가 볼린저밴드 하한선 밑에 있고 스토캐스틱 %K 값이 낮을 때 급락으로 판단) 텔레그램 메시지 발송
+* Notice: 산식이 맞는건지 모르게씀. 업비트에서 보는 값과 조금 오차가 있는 듯...
+"""
+
+##### 상수 ##################################################################################
+UPBIT_ACCESS = "uPbItAcCeSsToKeN"               # 업비트 Access Key
+UPBIT_SECRET = "uPbItSeCrEtToKeN"               # 업비트 Secret Key
+INTERVAL = "minute5"                            # 차트 종류
+
+TELEGRAM_TOKEN = "1234567890:tElEgRaMbOtToKeN"  # 텔레그렘 봇 토큰
+TELEGRAM_CHAT_ID = 123456789                    # 텔레그램 봇 아이디
+
+BB_COUNT = 20                                   # 볼린저 밴드(BB)의 길이
+BB_MULTIPLIER = 2                               # 볼린저 밴드(BB)에서 상하한선을 정하기 위해 사용하는 곱(승수)
+
+STOCH_COUNT = 20                                # 스토캐스틱(Stochastic)의 길이
+STOCH_UPPER_LIMIT = 0.8                         # 스토캐스틱(Stochastic) 상한선(Upper Limit)
+STOCH_LOWER_LIMIT = 0.1                         # 스토캐스틱(stochastic) 하한선(Lower Limit): 현재가가 이 밑으로 떨어지면 급락으로 간주
+############################################################################################
+
+##### 변수 ##################################################################################
+upbit = pyupbit.Upbit(UPBIT_ACCESS, UPBIT_SECRET)
+bot = telegram.Bot(TELEGRAM_TOKEN)
+tickers = ["KRW-BTC", "KRW-ETH", "KRW-ETC", "KRW-XRP", "KRW-XLM", "KRW-DOGE", "KRW-EOS", "KRW-ADA", "KRW-BCH", "KRW-LTC", "KRW-QTUM", "KRW-NEO", "KRW-BTG"]
+# 또는 tickers = cryptofunc.get_coin_tickers() 
+############################################################################################
+
+while True:
+    try: 
+        # 코인들 중에서
+        for ticker in tickers:
+            bb = cryptofunc.get_bb(ticker, INTERVAL, BB_COUNT, BB_MULTIPLIER)
+            stoch_k = cryptofunc.get_stoch_k(ticker, INTERVAL, STOCH_COUNT)
+            current_price = cryptofunc.get_current_price(ticker)
+
+            # 스토캐스틱 %K 값이 낮고 현재가가 볼린저 밴드 하한선 밑에 있고
+            if((stoch_k < STOCH_LOWER_LIMIT) and (current_price < bb["lbb"])):
+                balance = upbit.get_balance(ticker)
+                time.sleep(0.1)
+                order = upbit.get_order(ticker)
+                time.sleep(0.1)
+                
+                # 미보유 중이거나 거래 등록한 코인이 아닌 경우 급락 알림
+                if(not balance and not order):
+                    message = f"{ticker} Stochastic %K 값: {round(stoch_k, 3)}, Bollinger Bands 하한값: {round(bb['lbb'], 3)}, 현재가: {current_price}원으로 급락" 
+                    bot.sendMessage(TELEGRAM_CHAT_ID, text=message)
+
+    except Exception as e:
+        print(e)
+        time.sleep(1)
--- AutoTrade.py
+++ AutoTrade.py
@@ -2,18 +2,27 @@ import time
 import pyupbit
 import datetime
 import logging
+import cryptofunc
 from logging import handlers
 
+"""
+AutoTrade.py
+* Date: 2021.05.06.
+* Author: Jeon Won
+* Func: 변동성 돌파 전략을 사용하여 가상화폐를 자동 거래하는 파이썬 코드
+* Notice: 1일 차트 대신 4시간 차트로 변동성 돌파 전략 백테스트 해봤더니 오히려 손해날 확률이 더 높은듯...
+"""
+
 #############  상수  ##########################################################################
-ACCESS = "mYuPbItAcCeSsKeY"            # 업비트 Access Key
-SECRET = "mYuPbItSeCrEtKeY"            # 업비트 Secret Key
-COIN_TICKER = "KRW-BTC"                # 코인 티커
-COIN_NAME = "BTC"                      # 코인 이름
-INTERVAL = "minute240"                 # 차트 종류
-HOUR_TO_SELL = [0, 4, 8, 12, 18, 20]   # 변동성 돌파 전략 시 코인 매도 시간
-NOT_BUY_KRW = 1000000                  # 변동성 돌파 전략을 사용하지 않을 원화
-NOT_BUY_COIN = 0.123456                # 변동성 돌파 전략을 사용하지 않을 코인 수
-K = 0.5                                # 변동폭 계산에 사용되는 값
+ACCESS = "uPbItAcCeSsToKeN"           # 업비트 Access Key
+SECRET = "uPbItSeCrEtToKeN"           # 업비트 Secret Key
+COIN_TICKER = "KRW-BTC"               # 코인 티커
+COIN_NAME = "BTC"                     # 코인 이름
+INTERVAL = "minute240"                # 차트 종류
+HOUR_TO_SELL = [0, 4, 8, 12, 18, 20]  # 변동성 돌파 전략 시 코인 매도 시간
+NOT_BUY_KRW = 1000000                 # 변동성 돌파 전략을 사용하지 않을 원화
+NOT_BUY_COIN = 0.123456               # 변동성 돌파 전략을 사용하지 않을 코인 수
+K = 0.6                               # 변동폭 계산에 사용되는 값
 ################################################################################################
 
 
@@ -24,12 +33,6 @@ def get_target_price(ticker, interval, k):
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
-def get_ma(ticker, interval, count):
-    """이동 평균선 조회 (count: 일 수)"""
-    df = pyupbit.get_ohlcv(ticker, interval=interval, count=count)
-    ma = df['close'].rolling(count).mean().iloc[-1]
-    return ma
-
 def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
@@ -39,14 +42,16 @@ def get_balance(ticker):
                 return float(b['balance'])
             else:
                 return 0
-
-def get_current_price(ticker):
-    """현재가 조회"""
-    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 ###############################################################################################
 
 
-############# 로그 설정  ######################################################################
+#############  상수  ###########################################################################
+upbit = pyupbit.Upbit(ACCESS, SECRET)  # 업비트 객체
+ma5 = 0                                # 5일 이평선
+ma3 = 0                                # 3일 이평선
+###############################################################################################
+
+
 # log settings
 coinLogFormatter = logging.Formatter('%(asctime)s, %(message)s')
 
@@ -63,11 +68,6 @@ coinLogger.addHandler(coinLogHandler)
 # use logger
 print(f"{COIN_NAME} Auto trade start!")
 coinLogger.info(f"{COIN_NAME} Auto trade start!")
-###############################################################################################
-
-upbit = pyupbit.Upbit(ACCESS, SECRET)
-ma5 = 0
-ma3 = 0
 
 while True:
     try:
@@ -77,10 +77,11 @@ while True:
 
         # 차트 마감 10초 전에 코인을 현재가로 매도
         if((now.hour in HOUR_TO_SELL) and (start_time <= now <= end_time)):
-            ma5 = get_ma(COIN_TICKER, INTERVAL, 5)
-            ma3 = get_ma(COIN_TICKER, INTERVAL, 3)
+            ma5 = cryptofunc.get_ma(COIN_TICKER, INTERVAL, 5)
+            ma3 = cryptofunc.get_ma(COIN_TICKER, INTERVAL, 3)
             coin = get_balance(COIN_NAME) - NOT_BUY_COIN
-            current_price = get_current_price(COIN_TICKER)
+            current_price = cryptofunc.get_current_price(COIN_TICKER)
+
             # 최소 거래가능 코인을 보유해야 거래 가능
             if (coin > (1 / current_price * 5000)):
                 coinLogger.info(f"========== {COIN_NAME} 코인 {coin}개를 현재가로 매도합니다. ===========")
@@ -91,17 +92,19 @@ while True:
         else: 
             # 직전 차트의 이평선이 (3일 > 7일)인 경우
             if(ma3 > ma5): 
-                # 변동성 돌파 전략 수행
-                current_price = get_current_price(COIN_TICKER)
+                current_price = cryptofunc.get_current_price(COIN_TICKER)
                 target_price = get_target_price(COIN_TICKER, INTERVAL, K)
+
+                # 변동성 돌파 전략 수행
                 if(target_price < current_price): 
-                    # 최소 5,000원 이상 거래
                     krw = get_balance("KRW") - NOT_BUY_KRW
+
+                    # 최소 한화 5,000원 이상 거래
                     if(krw > 5000):
                         coinLogger.info(f"========== KRW {krw}원에 코인을 매수합니다. ==========")
                         coinLogger.info(upbit.buy_market_order(COIN_TICKER, krw))
                         coinLogger.info("========== 코인 매수 완료 ==========")
-        
+            
         time.sleep(5)
 
     except Exception as e:

14541daedb09159d50b6b3ffdc0a8db8b91f5ba0
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2021-05-21 17:01:47 +0900

    v0.2.0

--- cryptofunc.py
+++ cryptofunc.py
@@ -3,7 +3,7 @@ import time
 
 """
 cryptofunc.py
-* Date: 2021.05.20.
+* Date: 2021.05.21.
 * Author: Jeon Won
 * Func: 업비트 가상화폐 관련 정보를 얻어오기 위한 모듈
 * Notice: 산식을 맞게 적은건지 모르게씀. 업비트에서 보는 값과 조금 오차가 있을 수 있음...
@@ -57,7 +57,7 @@ def get_prev_ema(ticker, interval, count):
     return prev_ma
 
 def get_stoch_k(ticker, interval, count):
-    """스토캐스틱(Stochastic) 지표에서 사용되는 %K 값 구하기"""
+    """스토캐스틱(Stochastic)에서 사용되는 %K 값 계산"""
     df = pyupbit.get_ohlcv(ticker, interval, count)
     time.sleep(SLEEP_TIME)
 
@@ -71,23 +71,23 @@ def get_stoch_k(ticker, interval, count):
     prices.append(df.max()['low'])
     prices.append(df.max()['close'])
 
-    wave_min = min(prices)                    # 파동의 최저가격
-    wave_max = max(prices) - min(prices)      # 파동의 전체 폭
-    current_price = get_current_price(ticker) # 현재 가격
+    wave_min = min(prices)                     # 파동의 최저가격
+    wave_max = max(prices) - min(prices)       # 파동의 전체 폭
+    current_price = get_current_price(ticker)  # 현재 가격
     time.sleep(SLEEP_TIME)
     
     stoch_k = (current_price - wave_min) / wave_max
     return stoch_k
 
 def get_bb(ticker, interval, count, multiplier):
-    """볼린저 밴드에 사용되는 값(중심선, 상한선, 하한선)이 담긴 딕셔너리 반환"""
+    """볼린저 밴드에 사용되는 값(중심선, 상한선, 하한선) 계산"""
     df = pyupbit.get_ohlcv(ticker, interval, count)
     time.sleep(SLEEP_TIME)
     
-    std = df.std()['close']               # 종가 기준 표준편차
-    mbb = get_ma(ticker, interval, count) # 중심선 = 기간 내 이동평균선(저항자리)
-    ubb = mbb + std * multiplier          # 상한선 = 중심선 + 기간 내 표준편차 * 승수
-    lbb = mbb - std * multiplier          # 하한선 = 중신선 + 기간 내 표준편차 * 승수
+    std = df.std()['close']                # 종가 기준 표준편차
+    mbb = get_ma(ticker, interval, count)  # 중심선 = 기간 내 이동평균선(저항자리)
+    ubb = mbb + std * multiplier           # 상한선 = 중심선 + 기간 내 표준편차 * 승수
+    lbb = mbb - std * multiplier           # 하한선 = 중신선 + 기간 내 표준편차 * 승수
     time.sleep(SLEEP_TIME)
     
     dict_bb = {}
@@ -95,3 +95,21 @@ def get_bb(ticker, interval, count, multiplier):
     dict_bb["ubb"] = ubb
     dict_bb["lbb"] = lbb
     return dict_bb
+
+def get_fall_rate(ticker, interval, count):
+    """최근 차트 감소율 계산"""
+    df = pyupbit.get_ohlcv(ticker, interval, count)
+    time.sleep(SLEEP_TIME)
+    df_open = df["open"].to_list()
+    df_close = df["close"].to_list()
+    wave_max = max(df_open[count-1], df_close[count-1])
+    wave_min = min(df_open[count-1], df_close[count-1])
+
+    for i in reversed(range(count-1)):                                       # 최근 캔들 -> 과거 캔들 순으로 조사해서
+        if(df_open[i] >= df_close[i]):                                       # 하락 캔들을 발견하면
+            wave_max = df_open[i] if df_open[i] >= wave_max else wave_max    # 최대, 최소값 찾기
+            wave_min = df_close[i] if df_close[i] <= wave_min else wave_min
+        else:                                                                # 상승 캔들 발견 시
+            break                                                            # 더 이상 조사하지 않음
+    
+    return 1 - (wave_min / wave_max)                                         # 감소율 반환

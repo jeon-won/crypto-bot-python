fb511d304993dae7e6b799b0331b1af9572a3745
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2021-06-10 00:55:44 +0900

    v0.3.0
    AutoDetectFall.py 대폭 수정

--- AutoDetectFall.py
+++ AutoDetectFall.py
@@ -2,36 +2,40 @@ import cryptofunc
 import pyupbit
 import telegram
 import time
+import datetime
 
 """
 AutoDetectFall.py
-* Date: 2021.05.20.
+* Date: 2021. 6. 9.
 * Author: Jeon Won
-* Func: 가상화폐 급락했을 때(현재가가 볼린저밴드 하한선 밑에 있고 스토캐스틱 %K 값이 낮을 때 급락으로 판단) 텔레그램 알림 메시지 발송
+* Func: 다음 두 가지 조건 중 한 가지를 만족하면 텔레그램 메시지 전송
+  - 현재가가 볼린저밴드 하한선 밑에 있고 스토캐스틱 %K 값이 낮을 때
+  - 5분봉 3틱룰 적용 가능성이 있는 경우
 * Notice: 산식이 맞는건지 모르게씀. 업비트에서 보는 값과 조금 오차가 있는 듯...
 """
 
 ##### 상수 ##################################################################################
-UPBIT_ACCESS = "uPbItAcCeSsToKeN"                                  # 업비트 Access Key
-UPBIT_SECRET = "uPbItSeCrEtToKeN"                                  # 업비트 Secret Key
-INTERVAL = "minute5"                                               # 업비트 차트 종류
+INTERVAL = "minute5"                 # 차트 종류
+SLEEP_TIME = 0.1                     # API 호출 후 잠시 쉴 시간(초 단위)
+TICK_ESTI_RATE = 1                   # 캔들 크기가 이 비율보다 낮으면 1틱으로 산정(1은 0%)
 
-TELEGRAM_TOKEN = "1234567890:tElEgRaMbOtToKeN"                     # 텔레그렘 봇 토큰
-TELEGRAM_CHAT_ID = 123456789                                       # 텔레그램 봇 아이디
+TELEGRAM_TOKEN = "mYtElEgRaMtOkEn"  # 텔레그렘 봇 토큰
+TELEGRAM_CHAT_ID = 123456789        # 텔레그램 봇 아이디
 
-BB_COUNT = 20                                                      # 볼린저 밴드(BB)의 길이(일수?)
-BB_MULTIPLIER = 2                                                  # 볼린저 밴드(BB)에서 상하한선을 정하기 위해 사용하는 곱(승수)
+COUNT = 20                          # 분석할 차트의 최근 봉 개수
+BB_COUNT = 20                       # 볼린저 밴드(BB)의 길이
+BB_MULTIPLIER = 2                   # 볼린저 밴드(BB)에서 상하한선을 정하기 위해 사용하는 곱(승수)
 
-STOCH_COUNT = 20                                                   # 스토캐스틱(Stochastic)의 길이(일수?)
-STOCH_UPPER_LIMIT = 0.8                                            # 스토캐스틱(Stochastic) 상한선(Upper Limit)
-STOCH_LOWER_LIMIT = 0.1                                            # 스토캐스틱(stochastic) 하한선(Lower Limit): 현재가가 이 밑으로 떨어지면 급락으로 간주
+STOCH_COUNT = 20                    # 스토캐스틱(Stochastic)의 길이
+STOCH_UPPER_LIMIT = 0.8             # 스토캐스틱(Stochastic) 상한선(Upper Limit)
+STOCH_LOWER_LIMIT = 0.2             # 스토캐스틱(stochastic) 하한선(Lower Limit)
 ############################################################################################
 
 ##### 변수 ##################################################################################
-upbit = pyupbit.Upbit(UPBIT_ACCESS, UPBIT_SECRET)
 bot = telegram.Bot(TELEGRAM_TOKEN)
-tickers = ["KRW-BTC", "KRW-ETH", "KRW-ETC", "KRW-XRP", "KRW-XLM", "KRW-DOGE", "KRW-EOS", "KRW-ADA", "KRW-BCH", "KRW-LTC", "KRW-QTUM", "KRW-NEO", "KRW-BTG"]
-# 또는 tickers = cryptofunc.get_coin_tickers() 
+notified_tickers = []
+tickers = ["KRW-BTC", "KRW-ETH", "KRW-ETC", "KRW-XRP", "KRW-XLM", "KRW-DOGE", "KRW-EOS", "KRW-ADA", "KRW-BCH", "KRW-LTC", "KRW-QTUM"]
+# 또는 tickers = cryptofunc.get_coin_tickers()
 ############################################################################################
 
 while True:
@@ -41,21 +45,38 @@ while True:
             bb = cryptofunc.get_bb(ticker, INTERVAL, BB_COUNT, BB_MULTIPLIER)
             stoch_k = cryptofunc.get_stoch_k(ticker, INTERVAL, STOCH_COUNT)
             current_price = cryptofunc.get_current_price(ticker)
+
             # 스토캐스틱 %K 값이 낮고 현재가가 볼린저 밴드 하한선 밑에 있으면 급락 알림
             if((stoch_k < STOCH_LOWER_LIMIT) and (current_price < bb["lbb"])):
-                # balance = upbit.get_balance(ticker)
-                # time.sleep(0.1)
-                # order = upbit.get_order(ticker)
-                # time.sleep(0.1)
-                # if(not balance and not order):
-                message = f"{ticker} {current_price}원으로 급락"
+                if(notified_tickers.count(ticker) == 0):
+                    notified_tickers.append(ticker)
+                    message = f"{ticker} BB 하단 터치했습니다. (현재가: {current_price})"
+                    bot.sendMessage(TELEGRAM_CHAT_ID, text=message)
+
+            # 최근 차트에서 음봉 캔들을 3개 발견하면 3틱룰 가능성 알림
+            tick = 0
+            df = pyupbit.get_ohlcv(ticker, INTERVAL, COUNT)
+            time.sleep(SLEEP_TIME)
+            for i in range(COUNT-2, COUNT-7, -1):  # 최근 차트를 조사하여
+                open_price = df["open"][i]
+                close_price = df["close"][i]
+                rate = close_price / open_price
+                if(rate < TICK_ESTI_RATE):         # 음봉을 발견하면
+                    tick = tick + 1                # tick 값을 1 증가
+                else:                              # 양봉을 발견하면
+                    break                          # 더 이상 조사하지 않음
+            
+            if(tick >= 3 and notified_tickers.count(ticker) == 0):
+                notified_tickers.append(ticker)
+                message = f"{ticker} 3틱룰 적용 가능성이 있습니다. (현재가: {current_price})"
                 bot.sendMessage(TELEGRAM_CHAT_ID, text=message)
 
-            # 3% 이상 하락했을 때도 알림
-            fall_rate = cryptofunc.get_fall_rate(ticker, INTERVAL, 12)
-            if(fall_rate >= 0.03):
-                message = f"{ticker} {round(fall_rate, 3) * 100}% 하락"
-                bot.sendMessage(TELEGRAM_CHAT_ID, text=message)
+        # 5분마다 notified_tickers 초기화
+        now = datetime.datetime.now()
+        mod = divmod(now.minute, 5)[1]
+        is_multiple_5 = True if mod == 0 else False
+        if(is_multiple_5 and now.second < 10):
+            notified_tickers.clear()
 
     except Exception as e:
         print(e)
